bb97a431ca5e764bdfd00ad63554f81b79f9fb5e
David Jake Morfe <jakemorfe@gmail.com>
2022-01-08 01:28:03 -0500

    1.0.0

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-class App extends Component {
-  render() {
-    return (
-      <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-        <Text>Hello, world!</Text>
+const App = () => {
+  const [text, onChangeText] = useState();
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+      // Background Linear Gradient
+      colors={['#00E4F1', 'transparent']}
+      style={styles.background}
+      />
+      <View style={{marginBottom: 500}}>
+        <TextInput
+          style={styles.textinput}
+          placeholder="Enter your name..."
+          onChangeText={onChangeText}
+          defaultValue={text}
+        />
+        <Button
+          color="gold"
+          title="Start"
+        />
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default App;
@@ -21,8 +33,25 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FDC9CC',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 800,
+  },
+  textinput: {
+    borderWidth: 2,
+    borderColor: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 36,
+    color: '#000',
+    padding: 25,
   },
 });
