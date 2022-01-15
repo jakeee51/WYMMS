ba1fd5ff10751faf2d270c1a9afc5853f09543d8
David Jake Morfe <jakemorfe@gmail.com>
2022-01-15 03:12:16 -0500

    1.0.1
    Stable with coords

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-  const [text, onChangeText] = useState();
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={MainScreen}
+          options={{ title: 'LikeAlert' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const MainScreen = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location.coords.latitude);
+    text += ", " + JSON.stringify(location.coords.longitude)
+  }
+
   return (
     <View style={styles.container}>
       <LinearGradient
@@ -16,23 +50,22 @@ const App = () => {
       colors={['#00E4F1', 'transparent']}
       style={styles.background}
       />
-      <View style={{marginBottom: 500}}>
+      <View style={{width: 250, marginBottom: 200}}>
         <Button
-          color="gold"
-          title="Start"
+            color="gold"
+            title="Start"
         />
+        <Text>{text}</Text>
       </View>
     </View>
   );
-}
+};
 
-export default App;
+export default MyStack;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#FDC9CC',
@@ -42,6 +75,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
+    bottom: 0,
     height: 800,
   },
   textinput: {
