97934b12b169e9ff6ff42a40fdc73e8ea66dda0d
David Jake Morfe <jakemorfe@gmail.com>
2022-06-30 12:48:10 -0400

    2.0.0
    stable

--- Scripts/test_api.py
+++ Scripts/test_api.py
@@ -1,12 +1,48 @@
-import requests, json
+import requests, json, time, re
 
 obj = {"user": "S", "loc": "[0, 1]" , "pas": "S+J"}
-##obj = {"deed": "check"}
 
-url = "http://jakeee51.pythonanywhere.com/getloc"
-##url = "http://jakeee51.pythonanywhere.com/prp"
+set_url = "http://jakeee51.pythonanywhere.com/setloc"
+get_url = "http://jakeee51.pythonanywhere.com/getloc"
+prp_url = "http://jakeee51.pythonanywhere.com/prp"
 
-resp = requests.post(url, data=obj)
-##print("resp:", resp.text)
-vals = json.loads(resp.text)
-print(vals)
+cmds = """
+Commands:
+  set S
+  get
+  prp done
+  prp check
+"""
+cmd = ''
+
+while True:
+   obj = {"user": "S", "loc": "[0, 1]" , "pas": "S+J"}
+   cmd = input("Enter API command: ")
+   if "exit" in cmd:
+      break
+   elif "set" in cmd:
+      args = cmd.split(' ')
+      obj["user"] = args[1]
+      loc = input("Enter coordinates: ").replace(' ', '').strip('[]')
+      obj["loc"] = '[' + str(loc.replace(',', ', ')) + ']'
+      print(obj)
+      resp = requests.post(set_url, data=obj)
+      vals = json.loads(resp.text)
+      print(vals)
+   elif "get" in cmd:
+      if re.search(r"get \d+", cmd):
+         runs = int(cmd.split(' ')[1])
+      for i in range(runs):
+         resp = requests.post(get_url, data=obj)
+         vals = json.loads(resp.text)
+         print(vals); time.sleep(5)
+   elif "prp" in cmd:
+      args = cmd.split(' ')
+      obj = {"deed": ""}
+      if "done" in cmd or "check" in cmd:
+         obj["deed"] = args[1]
+      resp = requests.post(prp_url, data=obj)
+      vals = json.loads(resp.text)
+      print(vals)
+   else:
+      continue
